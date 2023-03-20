d842143bd9c17b0af1e54a73cbb766a2d5bdb5e5
Zehan Khan <zehan9211@gmail.com>
2023-03-20 16:32:19 +0530

    app path static

--- backend/server.js
+++ backend/server.js
@@ -5,25 +5,25 @@ const path = require('path')
 const app = express();
 const port = process.env.PORT || 3000;
 
-const appPath = __dirname+"/../frontend/dist/foodorder/index.html"
+const appPath = __dirname + "/../frontend/dist/"
 
 app.use(cors({
-    credentials:true,
-    origin:true
+    credentials: true,
+    origin: true
 }))
 
 app.use(express.static('../frontend/dist/foodorder'));
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.join(appPath))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(appPath, "foodorder", "index"))
 })
 
 
-app.get("/api",(req,res)=>{
+app.get("/api", (req, res) => {
     res.end("hello from backend")
 })
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server running on port ${port}`)
 })
