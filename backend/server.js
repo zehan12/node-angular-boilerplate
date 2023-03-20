e6afbc67457c6c4922a2960aecb28efddb82cafe
Zehan Khan <zehan9211@gmail.com>
2023-03-20 16:16:03 +0530

    add app-path

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require('path')
-
-
 const app = express();
 const port = process.env.PORT || 3000;
 
+const appPath = __dirname+"/../frontend/dist/foodorder/index.html"
+
 app.use(cors({
     credentials:true,
-    origin:["http://localhost:4200"]
+    origin:true
 }))
 
-app.use("/",express.static('../frontend/dist/foodorder'))
+app.use(express.static('../frontend/dist/foodorder'));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(""))
+})
+
 
 app.get("/api",(req,res)=>{
     res.end("hello from backend")
